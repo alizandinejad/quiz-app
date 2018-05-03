8a6a99bc58c57bef617e6fb81dc4589d5ceb62f0
Ali Zandi <me@alizandi.com>
2018-05-02 20:40:11 -1000

    another try

---
+++ js/content.js
@@ -0,0 +1,64 @@
+const responses = {
+  correct: ['Damn right!', 'Wow...You are smart!', 'BOOM! Nice.', 'Yea! Get it girl!', 'YES! Wait... Rick, is that you?' ],
+  incorrect: ['Um...no!', 'Nice try... I guess.', 'For real? No.', 'Smh bruh...', 'No. You been under a rock?', 'Geez...you suck.']
+};
+
+const quiz = {
+  questions: [
+  {
+    question: 'What is Scary Terry\'s catchphrase?',
+    a: 'I\'m your worste nightmare!',
+    b: 'Welcome to your nightmare, bitch!',
+    c: 'This is your nightmare!',
+    d: 'You can run, but you\'ll still die!',
+    answer: 'Welcome to your nightmare, bitch!',
+    img: 'scarry-terry.jpg',
+    img2: 'scarry-terry-2.jpg',
+    explanation: 'At first he first appears to be a bloodthirsty nightmare, but he is really just misunderstood. Scary Terry was very self-conscious about how scary he was and feared that he may not be terrifying enough to scare people, so he put a lot of pressure on himself because of that. This made him very tense and touchy, until Rick and Morty entered his dreams and made him feel better.'
+  },
+  {  
+    question: 'What dimension is Rick Sanchez from?',
+    a: 'Dimension C-500A',
+    b: 'Dimension 304-X',
+    c: 'Dimension C-137',
+    d: 'Dimension J19a7',
+    answer: 'Dimension C-137',
+    img: 'dimension.jpg',
+    img2: 'dimension-2.jpg',
+    explanation: 'Dimension C-137 is one of the many universes in the multiverse and the universe where the mainstream Rick and Morty are often identified as coming from. Given that Rick does not list his adoptive son-in-law Jerry as originating in the same dimension as him, dimension C-137 is the universe in which Earth was overrun by Cronenbergs in "Rick Potion #9".'
+  },
+  {  
+    question: 'What famous actress does Jerry admit to hating sleeping with in his alternate timeline where he is also famous?',
+    a: 'Hilary Swank',
+    b: 'Kristen Stewart',
+    c: 'Sarah Jessica Parker',
+    d: 'Rachel Dratch',
+    answer: 'Kristen Stewart',
+    img: 'jerry-smith.jpg',
+    img2: 'jerry-smith-2.jpg',
+    explanation: 'Jerry can sometimes become misguided by his insecurities. He frequently battles with bouts of insecurity and jealousy, being constantly reminded that his marriage is on rocky terms. He also has a habit of quitting when things get too difficult, which became problematic when he tasked a Mr. Meeseeks with helping him to improve his golf game.'
+  },
+  {  
+    question: 'What does Rick use to travel between dimensions and universes?',
+    a: 'Space laser',
+    b: 'Portal gun',
+    c: 'Intergalactic Travel Ray',
+    d: 'Universe  Saber',
+    answer: 'Portal gun',
+    img: 'traveler.jpg',
+    img2: 'traveler-2.jpg',
+    explanation: 'The Portal Gun is a gadget that allows the user(s) to travel between different universes/dimensions/realities. The Gun was likely created by a Rick, although it is unknown which one; if there is any truth to C-137\'s fabricated origin story, then he may not be the original inventor.'
+  },
+  {  
+    question: 'Who is Morty based on?',
+    a: 'Some loser nobody knows',
+    b: 'Hugo Strange',
+    c: 'Marty from Back To The Future',
+    d: 'Adam Webber from Blast From The Past',
+    answer: 'Marty from Back To The Future',
+    img: 'morty.jpg',
+    img2: 'morty-2.jpg',
+    explanation: 'Although the information is easily accesible, not much has been revealed about Morty\'s past life. Currently, Morty is 14 years old, and as of the beginning of the series, Rick Sanchez had been mysteriously missing from the family for a total of 14 years. It is unknown if Morty\'s birth and Rick\'s disappearance are linked to each other, but it has been shown on two seprate occasions that Rick knew Morty when he was only a baby'
+  },
+  ]
+};
